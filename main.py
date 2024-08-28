fe1a0efcbe322c9ec570959e99fdcf2899451011
= <zu212942@reddies.hsu.edu>
2024-08-27 20:20:11 -0500

    adding a char count function in main to count instances of single chars

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     book_path = "./books/frankenstein.txt"
     file_contents = get_book_text(book_path)
     word_count = count_words_in_text(file_contents)
-    print(word_count)
+    char_count = count_chars_in_text(file_contents)
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +13,20 @@ def get_book_text(path):
 def count_words_in_text(text):
     words = text.split()
     return len(words)
+
+def count_chars_in_text(text):
+    char_dict = {}
+    for word in text:
+        for char in word.lower(): 
+            if char in char_dict and char.isalpha():
+                char_dict[char] += 1
+            elif char not in char_dict and char.isalpha():
+                char_dict[char] = 1
+        
+
+    return char_dict
+           
+
+
 
 main()
